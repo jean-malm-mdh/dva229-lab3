481f38d4fc18052a30ff4abde6ef6c1f0232af90
Jean Malm <jean.malm@mdh.se>
2023-02-07 08:41:27 +0100

    Now with test scaffolding

---
+++ Lab3_Tests/UnitTest1.fs
@@ -0,0 +1,21 @@
+module Lab3_Tests
+
+open NUnit.Framework
+
+[<Test>]
+let ``Given Empty Sentence Then Interpreter Should Give Error`` () =
+    Assert.Fail("Not implemented yet")
+
+[<Test>]
+let ``Given Too Short Sentence Then Interpreter Should Give Error`` () =
+    Assert.Fail("Not implemented yet")
+
+[<Test>]
+let ``Given Too Long Sentence Then Interpreter Should Give Error`` () =
+    Assert.Fail("Not implemented yet")
+
+[<Test>]
+let ``Given a Noun Then Identifier Should Classify it as Noun`` () =
+    Assert.Fail("Not implemented yet")
+    
+//TODO: Continue adding tests
