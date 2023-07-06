432d30d3abe8ed91b162fe6468b99bfff8ddc6a8
Jean Malm <jean.malm@mdh.se>
2023-07-06 14:48:15 +0200

    added another test

--- Lab3_Tests/UnitTest1.fs
+++ Lab3_Tests/UnitTest1.fs
@@ -3,19 +3,23 @@ module Lab3_Tests
 open NUnit.Framework
 
 [<Test>]
-let ``Given Empty Sentence Then Interpreter Should Give Error`` () =
+let ``Given Empty Sentence Then Interpreter Shall Give Error`` () =
     Assert.Fail("Not implemented yet")
 
 [<Test>]
-let ``Given Too Short Sentence Then Interpreter Should Give Error`` () =
+let ``Given Too Short Sentence Then Interpreter Shall Give Error`` () =
     Assert.Fail("Not implemented yet")
 
 [<Test>]
-let ``Given Too Long Sentence Then Interpreter Should Give Error`` () =
+let ``Given Too Long Sentence Then Interpreter Shall Give Error`` () =
     Assert.Fail("Not implemented yet")
 
 [<Test>]
-let ``Given a Noun Then Identifier Should Classify it as Noun`` () =
+let ``Given a Noun Then Identifier Shall Classify it as Noun`` () =
+    Assert.Fail("Not implemented yet")
+
+[<Test>]
+let ``Given a Verb Then Identifier Shall Classify it as Verb`` () =
     Assert.Fail("Not implemented yet")
     
 //TODO: Continue adding tests
